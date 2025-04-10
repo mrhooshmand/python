48f0ae2bc21e813abd42ec531d1f419039e0bfff
hushmand <hushmand@safarbooking.com>
2025-04-10 14:05:33 +0330

    add selenium train file

--- selenium_training.py
+++ selenium_training.py
@@ -1,3 +1,16 @@
-from selenium import webdriver
+from time import sleep
 
-driver = webdriver.Chrome()
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
+options = webdriver.EdgeOptions()
+options.add_experimental_option("detach", True)
+driver = webdriver.Edge(options=options)
+driver.get("https://dashboard.flysepehran.com/login")
+driver.maximize_window()
+driver.find_element(By.ID, "user").send_keys('hooshmand')
+driver.find_element(By.ID, "password").send_keys('951623')
+driver.find_element(By.CLASS_NAME, "captcha_value").send_keys('25')
+sleep(1)
+driver.find_element(By.CLASS_NAME, "login-btn").click()
